dbb5ac1d641987309c06e5a110c5c98e92371e1e
Brandon <68145204+besser435@users.noreply.github.com>
2023-06-06 21:08:44 -0700

    Create server.py

---
+++ 1 billion gecs/server.py
@@ -0,0 +1,34 @@
+print(
+    "brandonusa's Stats: \n"
+    "Currently farming: Yes \n"
+    "Current balance: $morbillion \n"
+    "Average hourly gain: $220,000 \n"
+    "Average daily gain: $2,000,420 \n"
+    "Money gained over 24h: $morbtrillion \n"
+    "Balance change over 24h: +5% \n"
+    "Leaderboard position: 1 \n"
+    "Total time spent mining: 69h 69m \n"    
+)
+
+print(
+    "\nGeneral info: \n"
+    "Total money: $morbillion \n"
+    "Average (or mean?) money gain/h: $440,000 \n"
+)
+
+print(
+    "\nMeta info: \n"
+    "Global Kill flag status: False \n"
+
+    "Data received 1.69MB (last 24h): \n"
+    "Data sent (last 24h): 0.42MB \n"
+
+    "Data received 1.69MB (all-time): 90MB\n"
+    "Data sent 0.42MB (all-time): 53MB\n"
+
+    "Total data transferred (all-time): 0.42MB \n"
+    "Total data transferred (all-time): 0.42MB \n"
+
+    "Database size: 69MB \n"
+    "Uptime since restart: 1d 13h 3m \n"
+)
